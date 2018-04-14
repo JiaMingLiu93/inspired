e12c8a64f6312e37b229cb6fc6b41711d9f658a4
JiaMingLiu93 <626345665@qq.com>
2018-04-14 23:16:02 +0800

    add style for nav_section_items

--- pages/index/index.js
+++ pages/index/index.js
@@ -20,9 +20,9 @@ Page({
     duration: 1000,
     // nav 初始化
     navTopItems: fileData.getIndexNavData(),
-    // navSectionItems: fileData.getIndexNavSectionData(),
-    // curNavId: 1,
-    // curIndex: 0
+    navSectionItems: fileData.getIndexNavSectionData(),
+    curNavId: 1,
+    curIndex: 0
   },
   //事件处理函数
   bindViewTap: function() {
@@ -31,32 +31,10 @@ Page({
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    var that = this
+    that.setData({
+      list: that.data.navSectionItems
+    })
   },
   getUserInfo: function(e) {
     console.log(e)
--- utils/data.js
+++ utils/data.js
@@ -35,11 +35,140 @@ function getIndexNavData() {
   return arr
 }
 
+/*
+ * 首页 对应 标签 数据项
+ */
+function getIndexNavSectionData() {
+  var arr = [
+    [
+      {
+
+        subject: "秋季自然特价美甲",
+        coverpath: "../../images/recommend_img_01.png",
+        price: '¥198',
+        message: '我们追求的是没有最长只有更长！'
+      },
+      {
+
+        subject: "睫毛稀疏 种睫毛来帮忙",
+        coverpath: "../../images/recommend_img_02.png",
+        price: '¥1888',
+        message: '我们追求的是没有最长只有更长！'
+      },
+      {
+
+        subject: "爱丽克EircParisSalon",
+        coverpath: "../../images/recommend_img_03.png",
+        price: '¥1588',
+        message: '我们追求的是没有最长只有更长！'
+      },
+      {
+
+        subject: "伊本造型",
+        coverpath: "../../images/recommend_img_05.png",
+        price: '¥198',
+        message: '伊本造型是由著名形象设计师杨进先生创办。'
+      },
+      {
+
+        subject: " 画对了妆，微微一笑颜值倍儿高！",
+        coverpath: "../../images/recommend_img_06.png",
+        price: '¥198',
+        message: '《微微一笑很倾城》的剧照简直美腻到不要不要的'
+      }
+    ],
+    [
+      {
+        artype: 'nails',
+        subject: "秋季自然特价美甲",
+        coverpath: "../../images/recommend_img_01.png",
+        price: '¥198',
+        message: '我们追求的是没有最长只有更长！'
+      }
+    ],
+    [
+      {
+        artype: 'beauty',
+        subject: "爱丽克EircParisSalon",
+        coverpath: "../../images/recommend_img_03.png",
+        price: '¥1588',
+        message: '我们追求的是没有最长只有更长！'
+      },
+      {
+        artype: 'beauty',
+        subject: " 画对了妆，微微一笑颜值倍儿高！",
+        coverpath: "../../images/recommend_img_06.png",
+        price: '¥198',
+        message: '《微微一笑很倾城》的剧照简直美腻到不要不要的'
+      }
+    ],
+    [
+
+      {
+        artype: 'hair',
+        subject: "伊本造型",
+        coverpath: "../../images/recommend_img_05.png",
+        price: '¥1588',
+        message: '伊本造型是由著名形象设计师杨进先生创办。'
+      }
+    ],
+    [
+      {
+        artype: 'eyelash',
+        subject: "睫毛稀疏 种睫毛来帮忙",
+        coverpath: "../../images/recommend_img_02.png",
+        price: '¥1888',
+        message: '我们追求的是没有最长只有更长！'
+      }
+    ]
+  ]
+  return arr
+}
+/**
+ * 技师 数据
+ * */
+function getSkilledData() {
+  var arr = [
+    {
+      company: "西狮独品美容美发有限公司",
+      avatar: "../../images/skilledt_img_01.png",
+      nickname: '张技师',
+      price: '¥500',
+      message: '从事美发行业60余年，有丰富经验',
+      distance: '100m'
+    },
+    {
+      company: "圆月亮美甲沙龙",
+      avatar: "../../images/skilledt_img_02.png",
+      nickname: '包技师',
+      price: '¥800',
+      message: '从事美发行业60余年，有丰富经验',
+      distance: '200m'
+    },
+    {
+      company: "璀璨美睫会所",
+      avatar: "../../images/skilledt_img_03.png",
+      nickname: '王技师',
+      price: '¥600',
+      message: '从事美发行业60余年，有丰富经验',
+      distance: '100m'
+    },
+    {
+      company: "柔丝妮美容养生馆",
+      avatar: "../../images/skilledt_img_04.png",
+      nickname: '黄技师',
+      price: '¥800',
+      message: '从事美发行业60余年，有丰富经验',
+      distance: '400m'
+    }
+  ]
+  return arr
+}
 
 module.exports = {
   getBannerData: getBannerData,
   getIndexNavData: getIndexNavData,
-  // getIndexNavSectionData: getIndexNavSectionData,
+  getIndexNavSectionData: getIndexNavSectionData,
   // getPickerData: getPickerData,
   // getSkilledData: getSkilledData,
   // userData: userData,
